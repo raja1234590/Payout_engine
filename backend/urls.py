a5aa5af982344647ed060dd195afe8438a03a377
Gnana Prakash <gnanaprakashkovuru007@gmail.com>
2026-04-25 18:16:45 +0530

    Added home route

--- backend/urls.py
+++ backend/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
 
+def home(request):
+    return JsonResponse({"message": "Payout Engine API is running 🚀"})
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/v1/', include('core.urls')),
 ]
