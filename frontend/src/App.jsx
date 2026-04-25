21f54f570a038cf0fb555612b52955e7c3e46ae0
Gnana Prakash <gnanaprakashkovuru007@gmail.com>
2026-04-25 18:28:37 +0530

    Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE = 'http://127.0.0.1:8000/api/v1';
+const API_BASE = import.meta.env.VITE_API_BASE || '/api/v1';
 
 // We'll hardcode X-Merchant-Id to 1 for the challenge scope
 const axiosInstance = axios.create({
